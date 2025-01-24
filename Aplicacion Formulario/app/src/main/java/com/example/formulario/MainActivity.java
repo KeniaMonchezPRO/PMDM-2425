bbc9e4b69aa10562d7fecc6011a74fd49a8d5c38
IESPIRINGALLA\monoreken <jmonchez21.8@gmail.com>
2025-01-24 18:32:29 +0100

    acceso a datos con SharedPreferences

--- Aplicacion Formulario/app/src/main/java/com/example/formulario/MainActivity.java
+++ Aplicacion Formulario/app/src/main/java/com/example/formulario/MainActivity.java
@@ -2,15 +2,17 @@ package com.example.formulario;
 
 import static androidx.core.location.LocationManagerCompat.getCurrentLocation;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.TextView;
 import android.widget.Toast;
 import android.Manifest;
 
@@ -29,15 +31,12 @@ import com.google.android.gms.location.LocationServices;
 
 public class MainActivity extends AppCompatActivity {
 
-
     private Button gardar;
     private Button limpar;
     private EditText nome, apelidos, mobil, email, contrasinal;
     private CheckBox publicidade;
     private ImageButton imaxe;
     private Button axuda;
-
-
 
     //probando xeolocalizacion:
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1; //codigo de permiso
@@ -90,6 +89,10 @@ public class MainActivity extends AppCompatActivity {
                 publicidade.setChecked(false);
             }
         });
+
+        //probarPreferenciasCompartidas();
+        cargarDatosAlForm();
+
     }
 
     private void getCurrentLocation() {
@@ -131,5 +134,60 @@ public class MainActivity extends AppCompatActivity {
     public void startActivityHelp(View v) {
         Intent intent = new Intent(MainActivity.this, ActivityHelp.class);
         startActivity(intent);
+    }
+
+    public void probarPreferenciasCompartidas() {
+        //definimos la clase y creamos un editor q guarda la info
+        SharedPreferences prefs = this.getSharedPreferences("MisPreferencias", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        //metodos para guardar datos primitos en clave,valor
+        editor.putString("usuario", "monoreken");
+        editor.putInt("clave", 123);
+        editor.putBoolean("recordar_usuario", true);
+        //apply para guardar esos datos declarados e inicializados anteriormente
+        //dif entre apply e commit: si alguien intenta guardar muchos datos con SharedPreferences con put, tarda en persistirse, apply guarda en 2º plano, commit la guarda al momento, si tarda 2s en guardar la app "queda congelada"
+        editor.apply();
+
+        //leemos lo que hemos escrito, un valor por defecto en caso de que no lo encuentra o no exista el valor
+        //get el 2º valor es el por defecto
+        String nombreUsuario = prefs.getString("usuario", " ");
+        int claveUsuario = prefs.getInt("clave", 1234);
+        boolean recordarUsuario = prefs.getBoolean("recordarUsuario", false);
+
+        //inicializa el atributo nombre de nuestro form, el log nos añade esa linea en nuestro logCat
+        nome.setText(nombreUsuario);
+        Log.d("prefs", "String: " + nombreUsuario);
+        Log.d("prefs", "Int: " + claveUsuario);
+        Log.d("prefs", "Boolean: " + recordarUsuario);
+    }
+    public void cargarDatosAlForm() {
+           SharedPreferences prefs = this.getSharedPreferences("MisPreferencias",Context.MODE_PRIVATE);
+           SharedPreferences.Editor editor = prefs.edit();
+           editor.putString("nome","John");
+           editor.putString("apelido","Doe");
+           editor.putString("email","johndoe@gmail.com");
+           editor.putInt("mobil",123456789);
+           editor.putString("contrasinal","matrix10");
+           editor.putBoolean("publicidade",true);
+           editor.apply();
+           String nomeUsuario = prefs.getString("nome"," ");
+           String apelidoUsuario = prefs.getString("apelido"," ");
+           String emailUsuario = prefs.getString("email"," ");
+           int mobilUsuario = prefs.getInt("mobil",0);
+           String contrasinalUsuario = prefs.getString("contrasinal"," ");
+           boolean publicidadeUsuario = prefs.getBoolean("publicidade",false);
+
+           nome.setText(nomeUsuario);
+           apelidos.setText(apelidoUsuario);
+           email.setText(emailUsuario);
+           mobil.setText(mobilUsuario);
+           contrasinal.setText(contrasinalUsuario);
+           publicidade.setChecked(publicidadeUsuario);
+           Log.d("prefs","String: " + nomeUsuario);
+           Log.d("prefs","String: " + apelidoUsuario);
+           Log.d("prefs","String: " + emailUsuario);
+           Log.d("prefs","Int: " + mobilUsuario);
+           Log.d("prefs","String: " + contrasinalUsuario);
+           Log.d("prefs","Boolean: " + publicidadeUsuario);
     }
 }
