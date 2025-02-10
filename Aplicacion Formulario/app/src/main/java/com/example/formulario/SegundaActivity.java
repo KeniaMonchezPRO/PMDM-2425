900e528c14e8ef73b7b3f09d04a781aeb52c45eb
IESPIRINGALLA\monoreken <jmonchez21.8@gmail.com>
2025-02-10 21:50:44 +0100

    Tarea Fragment en activity_segunda

--- Aplicacion Formulario/app/src/main/java/com/example/formulario/SegundaActivity.java
+++ Aplicacion Formulario/app/src/main/java/com/example/formulario/SegundaActivity.java
@@ -2,12 +2,21 @@ package com.example.formulario;
 
 import android.os.Bundle;
 
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 public class SegundaActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda);
+
+        //configurando fragmentos
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.contenedor_fragment, new BottomMenuFragment());
+        transaction.commit();
     }
 
     @Override
