8645bb65225cd46977a3cdb069c3e615002c8273
IESPIRINGALLA\monoreken <jmonchez21.8@gmail.com>
2025-02-17 22:27:00 +0100

    cambios en la segunda activity

--- Aplicacion Formulario/app/src/main/java/com/example/formulario/SegundaActivity.java
+++ Aplicacion Formulario/app/src/main/java/com/example/formulario/SegundaActivity.java
@@ -2,13 +2,19 @@ package com.example.formulario;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import org.w3c.dom.Text;
+
 import Entities.Usuario;
 
 public class SegundaActivity extends BaseActivity {
+
+    private TextView nombre;
+    private TextView email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +33,11 @@ public class SegundaActivity extends BaseActivity {
         //si la ocmunicacion fue fructifera o no
         if(usuario != null) {
             Log.d("Usuario","None: " + usuario.getNombre() + ", Mail: " + usuario.getEmail());
+            nombre = findViewById(R.id.tv_nombre);
+            email = findViewById(R.id.tv_email);
+
+            nombre.setText(usuario.getNombre());
+            email.setText(usuario.getEmail());
         }
     }
 
